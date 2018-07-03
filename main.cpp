38a04e14feccc9f3df841d75d359149a00cb0bc4
Ray Redondo <rdrpenguin04@gmail.com>
2018-07-03 13:36:33 -0500

    Many improvements
    1. Tested and fixed current program
    2. Added one usage
    3. Built
    4. Added push.sh for easy git access

--- main.cpp
+++ main.cpp
@@ -1,30 +1,45 @@
+#include <stdlib.h>
+
 #include <ncurses.h>
 
-void help() {
-  wprintw(stdscr, "Usage: (WIP)"); // Rdr: I'll get back to this later.
+void quit() {
+	printw("Press any key to exit...");
+	refresh();
+	getch();
+	endwin();
+	exit(0);
+}
+
+void help(char* name) {
+	attron(COLOR_PAIR(1));
+	printw("Usage:\n");
+	printw("\t%s fetch <packagename>\n", name);
 }
 
 int main(int argc, char** argv) {
-  initscr();
-  start_color();
-  cbreak();
-  
-  // Initialize colors:
-  init_pair(1, COLOR_WHITE, COLOR_BLACK); // Standard color
-  init_pair(2, COLOR_RED, COLOR_BLACK); // Error color
-  
-  attron(COLOR_PAIR(1));
-  wprintw(stdscr, "Lightning Creations Unified Package Manager\n");
-  wprintw(stdscr, "by InfernoDeity and Rdrpenguin\n");
-  wprintw(stdscr, "Version 1.0\n");
-  wrefresh(stdscr);
-  
-  if(argc == 1) {
-    attron(COLOR_PAIR(2));
-    wprintw(stdscr, "ERROR! Not enough arguments!\n");
-    help();
-    return 1;
-  }
-  wprintw("WIP"); // Rdr: I'll get back to this later.
-  return 0;
+	initscr();
+	use_default_colors();
+	start_color();
+	cbreak();
+
+	// Initialize colors:
+	init_pair(1, COLOR_WHITE, -1); // Standard color
+	init_pair(2, COLOR_RED, -1); // Error color
+	init_pair(3, COLOR_GREEN, -1); // Confirmation color
+
+	attron(COLOR_PAIR(3));
+	printw("Lightning Creations Unified Package Manager\n");
+	attron(COLOR_PAIR(1));
+	printw("by InfernoDeity and Rdrpenguin\n");
+	printw("Version 1.0\n");
+	refresh();
+
+	if(argc == 1) {
+		attron(COLOR_PAIR(2));
+		printw("ERROR! Not enough arguments!\n");
+		help(argv[0]);
+		quit();
+	}
+	printw("WIP"); // Rdr: I'll get back to this later.
+	quit();
 }
