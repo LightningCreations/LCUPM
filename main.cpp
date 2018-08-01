548c6f617b487fd38796039570053f4aacd35dc1
InfernoDeity <chorman64@gmail.com>
2018-07-31 22:43:12 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -100,7 +100,7 @@ int main(int argc, char** argv) {
 				t.print("Press any key to exit>"s,endline).wait();
 				return 0;
 			}
-			t.print("Fetching"s, argv[2],"...",endline);
+			t.print("Fetching "s, argv[2],"...",endline);
 			startDownload(argv[2], "./download");
 			t.print("Download Complete"s,endline);
 		} else if(strcmp("help", argv[1]) == 0) {
